222cee04abee921eb316e314374416a66b04b376
eguzmanv <emguzmanvi@unal.edu.co>
2022-11-02 21:36:56 +0000

    station codes included

--- toolkits/get_waveforms_files.py
+++ toolkits/get_waveforms_files.py
@@ -1,5 +1,7 @@
 import os
 
+from obspy import read
+
 # --------------------------------------------------------
 def get_waveforms_files(path: str, format = None) -> list:
     '''
@@ -12,10 +14,26 @@ def get_waveforms_files(path: str, format = None) -> list:
                      Format type of the requested file (e.g. 'EVT', 'GCF', 'MSEED' ...)
 
     - Returns:
-        >>> flist : list
-                         List of paths of the requested file type
+        >>> fpaths : list
+                    List of paths of the requested file type
     '''
     
+    # File extensions
+    fextensions = {
+                   'MSEED': ['mseed', 'MSEED', 'seed'],
+                   'EVT'  : ['evt', 'EVT'],
+                   'GCF'  : ['gcf', 'GCF']
+                  }
+        # Get list of file extensions
+    fextensions_list = [ext for list_ in list(fextensions.values()) for ext in list_]
+
+    # Waveform formats
+    wv_formats = {
+                  'MSEED' : ['MSEED'],
+                  'EVT'   : ['KINEMETRICS_EVT'],
+                 }
+    wv_formats_list = [format for list_ in list(wv_formats.values()) for format in list_]
+
     fpaths = []                                                     # empty list of files paths
     
     # Loop: find paths of the files which end with '.format'
@@ -27,10 +45,21 @@ def get_waveforms_files(path: str, format = None) -> list:
             # Condition: file ends with '.format' or not
             if format != None: 
                 
-                if file.endswith(f'.{format}'):                  # e.g. if format = 'EVT', then the 'N001.EVT' file applies
+                if (format in fextensions_list) and (file.endswith(tuple(fextensions_list))):                  # e.g. if format = 'EVT', then the 'N001.EVT' file applies
                     fpaths.append(os.path.join(root_path, file))
+                    continue
                 
-                continue
+                elif (format in fextensions_list) and (not file.endswith(tuple(fextensions_list))):
+                    
+                    try:
+                        
+                        st = read(root_path, file)
+                        if (st[0].stats._format in wv_formats_list):
+                            pass
+                    
+                    except:
+                        continue
+
             else:
                 fpaths.append(os.path.join(root_path, file))
 
